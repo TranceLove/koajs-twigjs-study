4e1d5973f535ee4a3fa0c6437606b73c1f772faf
TranceLove <airwave209gt@gmail.com>
2016-07-05 12:41:57 +0800

    Add request module to demonstrate integration with external web service calls

--- server.js
+++ server.js
@@ -3,12 +3,24 @@
 const koa = require("koa");
 const Router = require("koa-router");
 const views = require("koa-views");
+const request = require("request");
 
 let app = koa();
 let router = new Router();
 
+process.on("uncaughtException", (err) => {
+    console.error(err)
+})
+
 let templateRenderTest = function *(){
+    let step = new Promise(function(resolve, reject) {
+        request("http://httpbin.org/status/202", (err, res, body) => {
+            resolve(res)
+        });
+    });
+    let result = yield step
     yield this.render("index", {
+        "response_from_google": result.statusCode,
         "current_time": new Date(),
         "random_number": Math.random()
     })
